5ba331ac907abbca630b7a1cafbaa8cfe9300168
Syed Abbas Abid <abbasshah493@gmail.com>
2018-12-10 19:31:58 +0500

    test

---
+++ projects/ngx-test/src/lib/ngx-test.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-NgxTest',
+  template: `
+    <p>
+      ngx-test works!
+    </p>
+  `,
+  styles: []
+})
+export class NgxTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/ngx-test/src/lib/ngx-test.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { NgxTestComponent } from './ngx-test.component';
+
+@NgModule({
+  declarations: [NgxTestComponent],
+  imports: [
+  ],
+  exports: [NgxTestComponent]
+})
+export class NgxTestModule { }
